bf600f2a3a0b3a4f75d479d75fb94a1814f61bc6
Hrishikesh Mothe <hrishikeshrm07@gmail.com>
2025-09-24 16:18:50 -0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,52 @@
+import streamlit as st
+import pandas as pd
+import requests
+import pickle
+
+# Load the processed data and similarity matrix
+with open('movie_data.pkl', 'rb') as file:
+    movies, cosine_sim = pickle.load(file)
+
+# Function to get movie recommendations
+def get_recommendations(title, cosine_sim=cosine_sim):
+    idx = movies[movies['title'] == title].index[0]
+    sim_scores = list(enumerate(cosine_sim[idx]))
+    sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
+    sim_scores = sim_scores[1:11]  # Get top 10 similar movies
+    movie_indices = [i[0] for i in sim_scores]
+    return movies[['title', 'movie_id']].iloc[movie_indices]
+
+# Fetch movie poster from TMDB API
+def fetch_poster(movie_id):
+    api_key = '7b995d3c6fd91a2284b4ad8cb390c7b8'  # Replace with your TMDB API key
+    url = f'https://api.themoviedb.org/3/movie/{movie_id}?api_key={api_key}'
+    response = requests.get(url)
+    data = response.json()
+    poster_path = data['poster_path']
+    full_path = f"https://image.tmdb.org/t/p/w500{poster_path}"
+    return full_path
+
+# Streamlit UI
+st.markdown("<h1 style='color: #1CE783;'>HULUR MOVIES</h1>", unsafe_allow_html=True)
+
+selected_movie = st.selectbox("Select a movie:", movies['title'].values)
+
+if st.button('Recommend'):
+    recommendations = get_recommendations(selected_movie)
+    
+    # Change color of "Top 10 recommended movies:" text
+    st.markdown("<h3 style='color: #1CE783;'>Top 10 recommended movies:</h3>", unsafe_allow_html=True)
+
+    # Create a 2x5 grid layout
+    for i in range(0, 10, 5):
+        cols = st.columns(5)
+        for col, j in zip(cols, range(i, i+5)):
+            if j < len(recommendations):
+                movie_title = recommendations.iloc[j]['title']
+                movie_id = recommendations.iloc[j]['movie_id']
+                poster_url = fetch_poster(movie_id)
+                with col:
+                    st.image(poster_url, width=130)
+                    
+                    # Change color of movie titles
+                    st.markdown(f"<span style='color: #FFFFFF;'>{movie_title}</span>", unsafe_allow_html=True)
